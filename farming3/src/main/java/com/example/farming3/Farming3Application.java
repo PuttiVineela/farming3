a60f7178bf683aeda9f2545f94862072c8549c17
Vineela Putti <vineelaputti18@gmail.com>
2023-12-05 17:42:54 +0530

    Your commit message herey

---
+++ farming3/src/main/java/com/example/farming3/Farming3Application.java
@@ -0,0 +1,13 @@
+package com.example.farming3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farming3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farming3Application.class, args);
+	}
+
+}
